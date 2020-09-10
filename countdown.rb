32791b138c8706164bb1aa5b46c17365daa73559
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-10 23:08:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,7 @@
-number = 10 
-while number > 0 
-puts "#{number} SECONDS!"
-number -= 1 
+number = 10
+ while number > 0
+   puts "#{number} SECOND(S)!"
+   number -= 1
+
+ return "HAPPY NEW YEAR!"  
 end
