78d31e8e03c875ed2cd7ed719618c4aaab542f67
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-10 23:21:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,5 +6,10 @@ end
 "HAPPY NEW YEAR!"
 end
 
-def countdown_to_sleep (sleep 1.seconds)
+def countdown_to_sleep (number)
+ while number > 0
+   puts "#{number} SECOND(S)!"
+   
+   number -= 1
 end
+"HAPPY NEW YEAR!"
